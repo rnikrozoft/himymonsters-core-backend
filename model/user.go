af8b4af4353818a702e2d2b521e98bd97db5edda
rnikrozoft <rnikrozoft.dev@gamil.com>
2024-09-20 18:23:51 +0700

    Add Wallet struct to model/user.go

--- model/user.go
+++ model/user.go
@@ -4,3 +4,8 @@ type RegisterWithDeviceID struct {
 	Username string `json:"username"`
 	Created  bool   `json:"created"`
 }
+
+type Wallet struct {
+	Coin   int64 `json:"coin"`
+	Dimond int64 `json:"dimond"`
+}
